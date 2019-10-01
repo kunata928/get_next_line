768273407fabe03ba80b934b5dd8d97107a1842e
kunata928 <kunata928@gmail.com>
2019-10-01 20:00:31 +0300

    1oct

--- get_next_line.c
+++ get_next_line.c
@@ -6,46 +6,42 @@
 /*   By: pmelodi <pmelodi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 22:12:01 by pmelodi           #+#    #+#             */
-/*   Updated: 2019/09/30 00:14:21 by pmelodi          ###   ########.fr       */
+/*   Updated: 2019/10/01 19:55:36 by pmelodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-t_gnl	*gnlnew(int fd)
+static t_gnl	*gnlnew(int fd)
 {
 	t_gnl	*list;
-	int		len;
 
-	len = 0;
 	if (!(list = (t_gnl *)ft_memalloc(sizeof(t_gnl))))
 		return (NULL);
 	list->str = (char *)malloc(sizeof(char));
 	list->str = "\0";
 	list->fd = fd;
-//	}
 	list->next = NULL;
-
 	return (list);
 }
 
 static t_gnl	*find_fd(int fd)
 {
-	t_gnl			*begin;
+	t_gnl			*list;
 	static t_gnl	*gnl = NULL;
 
-	begin = gnl;
-	while (begin)
+	list = gnl;
+	while (list)
 	{
-		if (begin->fd == fd)
-			return (begin);
-		begin = begin->next;
+		if (list->fd == fd)
+			return (list);
+		list = list->next;
 	}
-	if (!begin)
-		gnl = gnlnew(fd);
-	gnl->next = begin;
-	begin = gnl;
-	return (begin);
+	if (!list)
+		list = gnlnew(fd);
+	list->next = gnl;
+	gnl = list;
+	return (list);
 }
 
 static char		*str_slice(char *str)
@@ -72,11 +68,10 @@ static char		*str_slice(char *str)
 		i++;
 	}
 	res[i] = '\0';
-	//res = ft_strcpy(res, ft_strchr(str, '\n') + 1);
 	return (res);
 }
 
-static void		add_line(char **line, char *s)
+static char		*add_line(char **line, char *s)
 {
 	int len;
 
@@ -85,7 +80,7 @@ static void		add_line(char **line, char *s)
 	else
 		len = ft_strlen(ft_strchr(s, '\n'));
 	*line = ft_strsub(s, 0, ft_strlen(s) - len);
-	return ;
+	return (str_slice(s));
 }
 
 int				get_next_line(const int fd, char **line)
@@ -118,69 +113,8 @@ int				get_next_line(const int fd, char **line)
 	if (!ft_strlen(gnl->str))
 		return (0);
 	tmp = gnl->str;
-	add_line(line, gnl->str);
-	gnl->str = str_slice(gnl->str);
+	gnl->str = add_line(line, gnl->str);
+//	gnl->str = str_slice(gnl->str);
 	ft_strdel(&tmp);
 	return (1);
 }
-
-//int				get_next_line(const int fd, char **line)
-//{
-//	int		len;
-//	char	**buff[BUFF_SIZE + 1];
-//	t_gnl	*gnl;
-//	//char	*tmp;
-//
-//	if (fd < 0 || !line || read(fd, NULL, 0) < 0)
-//		return (-1);
-//	gnl = find_fd(fd);
-//	if (!gnl)
-//		return (0);
-//	while (!(ft_strchr(gnl->str, '\n')))
-//	{
-//		//add_line(line, gnl->str);
-//		if (!(*line))
-//			*line = ft_strdup(gnl->str);
-//		else
-//			*line = ft_strjoin(*line, gnl->str);
-//		ft_memdel((void **)&(gnl->str));
-//		if ((gnl->str = ft_strnew(BUFF_SIZE)))
-//		{
-//			if (!(len = read(fd, gnl->str, BUFF_SIZE)))
-//				return (1);
-//			gnl->str[len] = '\0';
-//		}
-//	}
-//	if (!(ft_strlen(gnl->str)))
-//		return (0);
-//	add_line(line, gnl->str);
-//	//tmp = gnl->str;
-//	gnl->str = str_slice(gnl->str);
-//	//ft_strdel(&tmp);
-//	//gnl->str = str_slice(gnl->str);
-//	return (1);
-//}
-
-
-//static void		add_line(char **line, char *s)
-//{
-//	char *tmp;
-//	int len;
-//
-//	if (!(ft_strlen(ft_strchr(s, '\n'))))
-//		len = 0;
-//	else
-//		len = ft_strlen(ft_strchr(s, '\n'));
-//	tmp = *line;
-//
-//	if (!(*line))
-//		*line = ft_strsub(s, 0, ft_strlen(s) - len);
-//	else
-////		if (!(ft_strchr(s, '\n')))
-////			*line = ft_strjoin(*line, s);
-////		else
-//		*line = ft_strjoin(*line, ft_strsub(s, 0, ft_strlen(s) - len));
-////	ft_strdel(&tmp);
-////		free(tmp);
-//	return ;
-//}
--- main.c
+++ main.c
@@ -8,7 +8,8 @@
 
 int				main(void)
 {
-	int		fd;
+	int		fd1;
+	int		fd2;
 	char 	*line;
 	int 	one;
 	int		i;
@@ -16,24 +17,24 @@ int				main(void)
 	i = 0;
 	one = 0;
 	//line = NULL;
-	if ((fd = open("/Users/pmelodi/Projects/GetNextLine/gnl1_3.txt", O_RDONLY)) == -1)
+	if ((fd1 = open("/Users/pmelodi/Projects/GetNextLine/gnl1_3.txt", O_RDONLY)) == -1)
 		ft_putendl("File error!");
-	while (get_next_line(fd, &line))
+	if ((fd2 = open("/Users/pmelodi/Projects/GetNextLine/readme.txt", O_RDONLY)) == -1)
+		ft_putendl("File error!");
+	while (get_next_line(fd1, &line))
 	{
 		printf("%s\n", line);
 		ft_strdel(&line);
+		if (get_next_line(fd2, &line))
+			printf("%s\n", line);
+		ft_strdel(&line);
 		i++;
 	}
-	close(fd);
+	close(fd1);
+	close(fd2);
 
-//	if ((fd = open("/Users/pmelodi/Projects/GetNextLine/readme.txt", O_RDONLY)) == -1)
-//		ft_putendl("File error!");
-//
-//	printf("%d\n", get_next_line(fd, &line));
-//	printf("%s\n", line);
-//	free(line);
-//
-//	close(fd);
+	while (1)
+		;
 	return (0);
 }
 
